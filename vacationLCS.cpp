ff627cd6734c1ce675e51a7fd4436e165a7ac16d
Martin Coria <martin.coria159250@alumnos.info.unlp.edu.ar>
2022-10-21 18:01:33 -0300

    manejo de entrada y salida falla

---
+++ vacationLCS.cpp
@@ -0,0 +1,49 @@
+/* Sample imput:
+	abcd
+	acdb
+	abcd
+	dacb
+	#
+	
+Sample output:
+Case #1: you can visit at most 3 cities.
+Case #2: you can visit at most 2 cities.
+*/
+
+
+
+
+#include<cstdio>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int lcs(string seq, string seq2);
+int main(){
+	int a,b;
+	case_n = 0
+	while (scanf("%s", &entrada), entrada != '#'){
+		case_n ++
+		seq_1 = entrada
+		scanf("%s", &entrada) 
+		seq_2 = entrada
+		
+		
+		result = lsc(seq1, se2)
+		
+		printf("Case #%d: you can visit at most %d cities", case_n, result);
+	
+	}
+	printf("%s\n %s", seq1, seq_2);
+
+int lcs(string seq, string seq2){
+	return "ok"
+}
+
+
+    
+
+
+	
+	return 0;
+}
