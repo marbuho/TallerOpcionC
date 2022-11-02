b1cd72800be47a58c219753f0bccef1c3cb5f9c6
Martin Coria <martin.coria159250@alumnos.info.unlp.edu.ar>
2022-11-02 01:45:39 -0300

    entrada grafo ok

--- grafo.cpp
+++ grafo.cpp
@@ -15,6 +15,8 @@ output
 #include <iostream>
 #include <vector>
 #include <algorithm>  // para usar funcion min
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -28,52 +30,48 @@ int main () {
     int n; // n: Cantidad de nodos,
     int v; // vertice v
     int w; // vertice ady (vecino)
-    scanf("%d", &n); // leer hasta que recibe un vertice v de valor = 0
-    		cout<< "n="<< n <<endl;
-    		scanf("%d", &v);
-    		
-    	    getiline(cin, line);
-    	    while(line != "0"){
-    	        stringstream ss(line);
-    	        ss >> v;
-    	        while(!ss.eof()){
-    	            ss >> u;
-    	            // conectar v con u
-    	        }
-    	         getiline(cin, line);
-    	    }
-    	    
-    		while (scanf("%d", &w), w != 0){
-    				cout<< "v="<< v <<endl;
-	    			cout<< "w="<< w <<endl;
-	    			cout<<"se hace el push en grafo:"<< v <<" push_back(w=" <<w <<")"<<endl;
-					grafo[v].push_back(w) ;// arma el grafo
-					cout<<"se hace el push en grafo:"<< w <<" push_back(w=" <<v <<")"<<endl;
-					grafo[w].push_back(v) ;// arma el grafo
-					
-			}
-    		cout<<"estoy aca"<<endl;
+    string line;
+    scanf("%d", &n);
+    while (n != 0){ // leer hasta que recibe un vertice v de valor = 0
+		cout<< "n="<< n <<endl;
 		
-    		
-    		
-	
-		
-		
+	    getline(cin, line);
+	    while(line != "0"){
+	        stringstream ss(line);
+	        ss >> v;
+	        while(!ss.eof()){
+	            ss >> w;
+	            cout<< "v="<< v <<endl;
+    			cout<< "w="<< w <<endl;
+    			cout<<"se hace el push en grafo:"<< v <<" push_back(w=" <<w <<")"<<endl;
+				grafo[v].push_back(w) ;// arma el grafo
+				cout<<"se hace el push en grafo:"<< w <<" push_back(w=" <<v <<")"<<endl;
+				grafo[w].push_back(v) ;// arma el grafo
+	        }
+	         getline(cin, line);
+	    }
+
 		//mostrar el grafo
 		cout<<"Grafo:"<<endl;
 	
 
-    for (int v = 0; v <= n; ++v) {
-        cout << "\n lista de ady del vertice " << v
-             << "\n V ";
-        for (auto x : grafo[v])
-            cout << "-> " << x;
-        printf("\n");
-    }
-
+	    for (int v = 0; v <= n; ++v) {
+	        cout << "\n lista de ady del vertice " << v
+	             << "\n V ";
+	        for (auto x : grafo[v])
+	            cout << "-> " << x;
+	        printf("\n");
+	    }
+	    
+	    for (auto& v : grafo) {
+			v.clear();
+		}
 		
+		scanf("%d", &n)	;
+
+	}
     	
-				 
+			 
     return 0;
 }
 
